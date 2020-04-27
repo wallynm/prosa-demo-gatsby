ee98fff43b649ea67aa32c87834d63698dc36721
Wallysson Nunes <wally.nm@gmail.com>
2020-04-27 18:52:53 -0300

    Initial files

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter New age', // <title>
+  manifestName: 'newage',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-newage/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/theanubhav/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+};
